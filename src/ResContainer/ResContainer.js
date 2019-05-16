ab2dc36198fcfc54a9c7ae2530f47160018d2478
B3an5 <beantheo@gmail.com>
2019-05-16 10:37:19 -0600

    reservation card style

--- src/ResContainer/ResContainer.js
+++ src/ResContainer/ResContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ResCard from '../ResCard/ResCard'
+import './ResContainer.css'
 
 export default function ResContainer(props) {
 
 
   return(
-    <div>
+    <div className='res-container'>
       {props.resArr.map((res) => {
         return <ResCard deleteRes={props.deleteRes} res={res} />
       })}
